29325516c86616fb6a265eb93c2d6636815d0832
o0kam1 <duzhaokun2@gmail.com>
2026-06-21 21:05:41 +0800

    feat(modules): add PegasusItemFilter

---
+++ modules/src/main/java/dev/o0kam1/ui/PegasusItemFilter.kt
@@ -0,0 +1,111 @@
+package dev.o0kam1.ui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.View
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.Switch
+import android.widget.TextView
+import io.github.duzhaokun123.module.base.ModuleEntry
+import io.github.duzhaokun123.yabr.module.UICategory
+import io.github.duzhaokun123.yabr.module.base.BaseModule
+import io.github.duzhaokun123.yabr.module.base.SwitchModule
+import io.github.duzhaokun123.yabr.module.base.UIComplex
+import io.github.duzhaokun123.yabr.module.base.isEnabled
+import io.github.duzhaokun123.yabr.module.core.ConfigStore
+import io.github.duzhaokun123.yabr.module.core.PegasusHook
+import io.github.duzhaokun123.yabr.utils.ModuleEntryTarget
+import io.github.duzhaokun123.yabr.utils.Toast
+import io.github.duzhaokun123.yabr.utils.getJsonFieldValueAs
+import io.github.duzhaokun123.yabr.utils.loadMethod
+
+data class PegasusHolderInfo(
+    val type: String,
+    val bizType: Any?,
+    val className: String
+)
+
+@ModuleEntry(
+    id = "dev.o0kam1.ui.PegasusItemFilter",
+    targets = [ModuleEntryTarget.MAIN]
+)
+object PegasusItemFilter : BaseModule(), SwitchModule, UIComplex {
+    override val name = "首页卡片过滤"
+    override val description = "移除指定类型卡片"
+    override val category = UICategory.UI
+
+    val pegasusHolderTypes = mutableMapOf<String, PegasusHolderInfo>()
+
+    override fun onLoad(): Boolean {
+        val method_PegasusHolderData_getHolderType =
+            loadMethod("Lcom/bilibili/pegasus/PegasusHolderData;->getHolderType()Ljava/lang/String;")
+        val method_PegasusHolderData_getBizType =
+            loadMethod("Lcom/bilibili/pegasus/PegasusHolderData;->getBizType()Lcom/bilibili/pegasus/BizType;")
+        val config = ConfigStore.ofModule(this)
+        PegasusHook.addInterceptFirst(id) { data ->
+            val toRemove = config.getStringSet("remove") ?: emptySet()
+            val items = data.getJsonFieldValueAs<MutableList<Any>>("items")
+            items.removeAll { item ->
+                val itemClass = item.javaClass
+                val type = method_PegasusHolderData_getHolderType.invoke(item) as String
+                val bizType = method_PegasusHolderData_getBizType.invoke(item)
+                pegasusHolderTypes[type] = PegasusHolderInfo(type, bizType, itemClass.name)
+                type in toRemove
+            }
+        }
+        return true
+    }
+
+    override fun onUnload(): Boolean {
+        PegasusHook.removeIntercept(id)
+        return super.onUnload()
+    }
+
+
+
+    override fun onCreateUI(context: Context): View {
+        val config = ConfigStore.ofModule(this)
+        val toRemove = (config.getStringSet("remove") ?: emptySet()).toMutableSet()
+
+        val ll = LinearLayout(context).apply {
+            orientation = LinearLayout.VERTICAL
+        }
+
+        @SuppressLint("UseSwitchCompatOrMaterialCode")
+        val sw = Switch(context).apply {
+            text = "启用首页卡片过滤"
+            isChecked = this@PegasusItemFilter.isEnabled
+            setOnCheckedChangeListener { _, isChecked ->
+                this@PegasusItemFilter.isEnabled = isChecked
+                if (isChecked && pegasusHolderTypes.isEmpty()) {
+                    Toast.show("刷新首页以获取数据")
+                }
+            }
+        }
+        ll.addView(sw)
+        for (type in (pegasusHolderTypes.keys + toRemove)) {
+            val cb_item = CheckBox(context).apply {
+                @SuppressLint("SetTextI18n")
+                text = "$type\n${pegasusHolderTypes[type]}"
+                isChecked = type in toRemove
+                setOnCheckedChangeListener { _, isChecked ->
+                    if (isChecked) {
+                        toRemove.add(type)
+                    } else {
+                        toRemove.remove(type)
+                    }
+                    config.putStringSet("remove", toRemove)
+                }
+            }
+            ll.addView(cb_item)
+        }
+
+        ll.addView(TextView(context).apply {
+            @SuppressLint("SetTextI18n")
+            text = "显然如果所有类型都被过滤导致加载内容为空集首页加载会出错"
+        })
+
+        return ll
+    }
+}
